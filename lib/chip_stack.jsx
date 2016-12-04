5c7b5e88bb380867ec22c78ec871ec877014e37f
Dustin Morrill <dmorrill10@gmail.com>
2016-12-04 00:02:15 -0700

    Add ChipStack component

---
+++ lib/chip_stack.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class ChipStack extends React.Component {
+  render() {
+    return (
+      <div className='chip_stack'>
+        <div className='amount'>this.props.amount</div>
+      </div>
+    );
+  }
+}
+module.exports = ChipStack;
---
+++ test/chip_stack.test.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+const ChipStack = require('../dist/chip_stack.js');
+
+test('Works', () => {
+  const patient = <ChipStack amount='9001'/>;
+  const tree = renderer.create(patient).toJSON();
+  expect(tree).toMatchSnapshot();
+});
