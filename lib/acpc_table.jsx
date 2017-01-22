6cbe5671e225125cf6b103a7fee71b22c60a5e49
Dustin Morrill <dmorrill10@gmail.com>
2017-01-21 21:25:52 -0700

    Add AcpcTable

--- lib/acpc_table.jsx
+++ lib/acpc_table.jsx
@@ -1,5 +1,48 @@
-module.exports = sum
+import React from 'react';
+import {CommunityInformation} from './community_information';
+import {_, User, Opponent} from './player';
 
-function sum(a, b) {
-  return a + b;
+class AcpcTable extends React.Component {
+  communityInformation() {
+    return (<CommunityInformation
+      key='community-information'
+      cards={this.props.boardCards}
+      potChips={this.props.potChips}/>);
+  }
+  user() {
+    return (<User
+      key='user'
+      name={this.props.user.name}
+      hole-cards={this.props.user.holeCards}
+      dealer={this.props.user.dealer}
+      acting={this.props.user.acting}
+      chip-stack-amount={this.props.user.chipStackAmount}
+      winnings={this.props.user.winnings}
+      contribution={this.props.user.contribution}/>);
+  }
+  opponents() {
+    let o = [];
+    for (let i = 0; i < this.props.opponents.length; ++i) {
+      let opponent = this.props.opponents[i];
+      o.push(
+        <li key={i}><Opponent
+          name={opponent.name}
+          hole-cards={opponent.holeCards}
+          dealer={opponent.dealer}
+          acting={opponent.acting}
+          chip-stack-amount={opponent.chipStackAmount}
+          winnings={opponent.winnings}
+          contribution={opponent.contribution}/></li>
+      );
+    }
+    return <ol key='opponents' className='opponents'>{o}</ol>;
+  }
+  render() {
+    let l = [this.opponents(), this.communityInformation(), this.user()];
+    return (
+      <div className='acpc-table'>{l}</div>
+    );
+  }
 }
+
+export {AcpcTable};
--- test/acpc_table.test.jsx
+++ test/acpc_table.test.jsx
@@ -1,5 +1,60 @@
-const sum = require('../dist/acpc_table.js');
+import React from 'react';
+import renderer from 'react-test-renderer';
+import {shallow} from 'enzyme';
+import {AcpcTable} from '../dist/acpc_table.js';
 
-test('adds stuff', () => {
-  expect(sum(1, 2)).toBe(3)
-})
+describe('AcpcTable', () => {
+  describe("with 2-player Texas hold'em table on the river", () => {
+    it('works', () => {
+      const boardCards = [
+        {
+          rank: '2',
+          suit: '&hearts;'
+        }, {
+          rank: '3',
+          suit: '&diamonds;'
+        }, {
+          rank: '10',
+          suit: '&spades;'
+        }, {
+          rank: 'A',
+          suit: '&clubs;'
+        }, {
+          rank: 'K',
+          suit: '&diamonds;'
+        }
+      ];
+      const user = {
+        name: 'player1',
+        holeCards: [
+          {}, {}
+        ],
+        dealer: true,
+        acting: true,
+        chipStackAmount: 1000,
+        winnings: 0,
+        contribution: 400
+      };
+      const opponents = [
+        {
+          name: 'player2',
+          holeCards: [
+            {}, {}
+          ],
+          dealer: false,
+          acting: false,
+          chipStackAmount: 800,
+          winnings: 0,
+          contribution: 600
+        }
+      ];
+      const patient = (<AcpcTable
+        boardCards={boardCards}
+        potChips={1000}
+        user={user}
+        opponents={opponents}/>);
+      const wrapper = shallow(patient);
+      expect(renderer.create(patient).toJSON()).toMatchSnapshot();
+    });
+  });
+});
