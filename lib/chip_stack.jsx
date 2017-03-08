22192e5bbbf7abebf2415bfddf9e93b8a8267d57
Dustin Morrill <dmorrill10@gmail.com>
2017-03-07 17:03:19 -0700

    Separate chip amount and its image in ChipStack.

--- lib/chip_stack.jsx
+++ lib/chip_stack.jsx
@@ -3,9 +3,14 @@ import React from 'react';
 class ChipStack extends React.Component {
   render() {
     if (this.props.amount && this.props.amount > 0) {
+      const l = [(
+          <div key='amount' className='amount'>{this.props.amount}</div>
+        ), (
+          <div key='image' className='image'></div>
+        )];
       return (
         <div className='chip-stack'>
-          <div className='amount'>{this.props.amount}</div>
+          {l}
         </div>
       );
     } else {
